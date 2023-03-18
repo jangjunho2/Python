ba8b2da2e2f900220fd4f14b8e860fcdb7d3c8da
jangjunho2 <104313883+jangjunho2@users.noreply.github.com>
2023-03-18 23:53:29 +0900

    얼음...

--- ThisIsCodingTest/DFS,BFS/음료수_얼려_먹기.py
+++ ThisIsCodingTest/DFS,BFS/음료수_얼려_먹기.py
@@ -5,9 +5,24 @@
 
 '''
 (0)얼음 틈을 찾는 함수 (왼쪽위부터 찾음)
-#(1)칸막이 or (0)방문한거 visited True 확인하기. 방문한 얼음이면 패스
+#(1)칸막이 or (0)방문한거 visited True 확인하기. 방문한 얼음이면 continue
 틈이 있는 경우 dfs함수 호출 >> 틈 찾기 이어서 진행... 한번 만 돌면 됨
+'''
 
+
+def find_crack():
+    global ice
+
+    for i in range(row):
+        for j in range(col):
+            if array[i][j] == 1 or visited[i][j]:  # 칸막이거나 방문한곳이면 continue
+                continue
+            else:
+                dfs(array, i, j)
+                ice += 1
+
+
+'''
 dfs함수
 상하좌우를 스택에 담음 #단, 담을수 없는 경우: (1) 칸막이 or 존재 하지 않는 좌표면 무시함
 스택에 있는 것들을 방문 #visited true해주기
@@ -16,27 +31,31 @@ dfs함수
 '''
 
 
-def dfs(array, visited):
-    pass  # 구현 끝나면 지울것
-    ice = 0  # 아이스크림 수
+def dfs(array, r, c):
+    visited[r][c] = True
 
-
-def 틈을찾는함수():
-    pass
+    drow = [0, 0, -1, 1]
+    dcol = [1, -1, 0, 0]
+    dir = [0, 1, 2, 3]
+    for i in range(len(dir)):
+        nrow = r+drow[i]
+        ncol = c+dcol[i]
+        if nrow >= 0 and ncol >= 0 and nrow < row and nrow < row:  # 좌표 안에 있다면?
+            if visited[nrow][ncol] == 0:  # 방문하지 않았다면?
+                dfs(array, nrow, ncol)  # 방문 ㄱ
 
     # n, m 입력받기
-n, m = map(int, input().split())
+row, col = map(int, input().split())
 
 # 방향정의
-direction = [0, 1, 2, 3]  # 상하좌우
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-
 
 # 얼음 생성  # (0)은 얼음  (1)은 칸막이
-array = [list(map(int, input())) for _ in range(n)]
-# print(array)
+array = [list(map(int, input())) for _ in range(row)]
+print(array)
 
-visited = [[False]*n]*m  # 얼음 크기만큼 방문한곳 기록
+visited = [[False]*col for _ in range(row)]  # 얼음 크기만큼 방문한곳 기록
+print(visited)
+ice = 0  # 아이스크림 수
 
-dfs(array, visited)
+find_crack()  # 시작
+print(ice)  # 정답 출력
